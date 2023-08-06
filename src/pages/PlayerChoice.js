8a8e5103a5722c8c8e424b13ec1a2d415572e34a
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-06 20:12:08 +0200

    Add page PlayerChoice

---
+++ src/pages/PlayerChoice.js
@@ -0,0 +1,39 @@
+// file PlayerChoice.js
+
+import React from "react"
+
+import PageTitle from './page-title';
+
+// Main content of the page
+export default function Welcome() {
+
+    return (
+        <div className="welcome ms-5 me-5">
+            <PageTitle title="Spielerwahl" subtitle="Wer spiel mit?" />
+
+            <div class="card mt-3">
+                <div class="card-body d-flex flex-column align-items-center"></div>
+                <p className="text-center mb-2 fs-5">Hier kannst du festlegen wieviele Spieler mitmachen und wie du sie nennst.</p>
+                <p className="text-center mb-2 fs-5">(1 Spieler ist voreingestellt und heißt Sonne.)</p>
+            </div>
+
+            {/* Spieler */}
+
+            <div class="card mt-3">
+                <div class="card-body d-flex flex-column align-items-center">
+                    <h4 class="card-title mb-4">Möchtest du Spieler auswählen?</h4>
+                    <button id="addPlayerButton" class="btn btn-primary">Spieler wählen</button>
+                </div>
+            </div>
+
+            <div class="card mt-3">
+                <div class="card-body d-flex flex-column align-items-center">
+                    <h4 class="card-title mb-4">Starte das nächste Spiel.</h4>
+                    <button id="startNewGameButton" class="btn btn-primary">Spielen!</button>
+                </div>
+            </div>
+
+        </div>
+
+    );
+}
