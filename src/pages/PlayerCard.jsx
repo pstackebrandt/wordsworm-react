5fd0a3f96709c44df921a24d3b24d158dca2c639
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-07 17:47:50 +0200

    PlayerChoice: Fix update of player name player uses unique to identify player Update text syntax

--- src/pages/PlayerCard.jsx
+++ src/pages/PlayerCard.jsx
@@ -1,28 +1,22 @@
 // file PlayerCard.jsx
 
-// Import required React hooks and the GameContext.
 import React, { useState, useContext } from 'react';
 import GameContext from './GameContext';
 
 function PlayerCard({ player }) {
-    // Local state to hold and update the player name input.
+    // Local state for the player's name input value.
     const [inputValue, setInputValue] = useState(player.name);
 
-    // Destructure the required values from the GameContext.
-    const { players, addPlayer } = useContext(GameContext);
+    // Get the updatePlayer function from the GameContext.
+    const { updatePlayer } = useContext(GameContext);
 
-    // Function to handle changing of player's name.
+    // Update the player's name when it changes.
     const handleNameChange = () => {
-        // Check if the input has a value and if it's different from the current player name.
         if (inputValue && inputValue !== player.name) {
-            // Update the player's name within the list of players.
-            const updatedPlayers = players.map(p => p.name === player.name ? { ...p, name: inputValue } : p);
-            // Update the list of players.
-            addPlayer(updatedPlayers);
+            updatePlayer(player.id, { name: inputValue });
         }
     }
 
-    // Render the player card.
     return (
         <div className="card mt-3">
             <div className="card-body d-flex flex-column align-items-center">
@@ -31,16 +25,12 @@ function PlayerCard({ player }) {
                     <input 
                         id="playerNameInput"
                         type="text"
-                        // Placeholder text translates to "New player name".
                         placeholder="Neuer Spielername"
                         className="form-control m-2"
                         value={inputValue}
-                        // Update inputValue when the input changes.
                         onChange={(e) => setInputValue(e.target.value)}
-                        // If 'Enter' key is pressed, call the name change handler.
                         onKeyPress={(e) => e.key === 'Enter' && handleNameChange()}
                     />
-                    {/* // Button to manually trigger name change. */}
                     <button 
                         className="btn btn-secondary m-2"
                         onClick={handleNameChange}
@@ -48,12 +38,11 @@ function PlayerCard({ player }) {
                         Name ändern
                     </button>
                 </div>
-                {/* // Button to delete the player (functionality not yet implemented). */}
+                {/* Delete player functionality yet to be implemented. */}
                 <button className="btn btn-danger">Spieler löschen</button>
             </div>
         </div>
     );
 }
 
-// Export the PlayerCard component.
 export default PlayerCard;
