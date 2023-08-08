7cc877d2b96516d2bef5b60e8c7b60f6a10be989
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-08 13:34:57 +0200

    Player choice: Add player: temp, buggy implementation

--- src/pages/GameProvider.jsx
+++ src/pages/GameProvider.jsx
@@ -3,13 +3,18 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
+// Components
 import GameContext from './GameContext';
+
+const generateDefaultPlayerName = () => `Guest ${Math.floor(Math.random() * 1000)}`;
 
 const defaultPlayer = {
     id: uuidv4(),
-    name: "Gast",
-    foundWords: [],
-    Punkte: 0,
+    name: generateDefaultPlayerName(),
+    // foundWords: [], // no default words for production
+    foundWords: ["Anton", "Nena", "Ast"], // default words for testing
+    // Score: 300, // no default score for production
+    Score: 300, // default score for testing
 };
 
 // use of GameProvider see at end of this chat: https://chat.openai.com/share/9decdb18-a8f4-4783-8f99-4a69dbcc903a
@@ -23,29 +28,43 @@ const GameProvider = ({ children }) => {
     };
 
     const addPlayer = (player) => {
-        setPlayers(prevPlayers => [...prevPlayers, player]);
+        if (player) {
+            setPlayers(prevPlayers => [...prevPlayers, player]);
+            console.log("Player added: ", player);
+        }
+    };
+
+    const addDefaultPlayer = () => {
+        const defaultPlayer = {
+            id: uuidv4(),
+            name: generateDefaultPlayerName(),
+            foundWords: [],
+            Score: 0,
+        }
+
+        addPlayer(defaultPlayer);
+    };
+
+    const updatePlayer = (playerId, updatedPlayer) => {
+        setPlayers(prevPlayers =>
+            prevPlayers.map(player =>
+                player.id === playerId ? { ...player, ...updatedPlayer } : player
+            )
+        );
     };
 
     const updatePlayers = (newPlayers) => {
         setPlayers(newPlayers);
     };
 
-    const updatePlayer = (playerId, updatedPlayer) => {
-        setPlayers(prevPlayers => 
-            prevPlayers.map(player => 
-                player.id === playerId ? { ...player, ...updatedPlayer } : player
-            )
-        );
-    };
-    
-
     const value = {
         words,
         players,
         addWord,
-        addPlayer, 
+        addPlayer,
+        addDefaultPlayer,
+        updatePlayer,
         updatePlayers,
-        updatePlayer
     };
 
     return (
--- src/pages/PlayerChoice.jsx
+++ src/pages/PlayerChoice.jsx
@@ -4,12 +4,17 @@ import React, {useContext} from "react"
 import GameContext from "./GameContext";
 import { Link } from "react-router-dom";
 
+// page components
 import PageTitle from './PageTitle';
 import PlayerCard from './PlayerCard'
 
 // Main content of the page
 export default function PlayerChoice() {
-    const { players } = useContext(GameContext);
+    const { players, addDefaultPlayer } = useContext(GameContext);
+
+    const handleAddPlayer = () => {
+        addDefaultPlayer();
+    };
     
     return (
         <div className="welcome ms-5 me-5">
@@ -26,7 +31,7 @@ export default function PlayerChoice() {
             <div className="card mt-3">
                 <div className="card-body d-flex flex-column align-items-center">
                     <h4 className="card-title mb-4">Möchtest du einen Spieler hinzufügen?</h4>
-                    <button id="addPlayerButton" className="btn btn-primary">Noch ein Spieler!</button>
+                    <button id="addPlayerButton" className="btn btn-primary" onClick={handleAddPlayer}>Noch ein Spieler!</button>
                 </div>
             </div>
 
