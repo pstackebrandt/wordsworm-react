9375e4b8c6269552e1e50639388411674191142c
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-08 15:16:40 +0200

    GameProvider use process.env.NODE_ENV to add test data to defaultPlayer in development environment

--- src/pages/GameProvider.jsx
+++ src/pages/GameProvider.jsx
@@ -6,15 +6,14 @@ import { v4 as uuidv4 } from 'uuid';
 // Components
 import GameContext from './GameContext';
 
-const generateDefaultPlayerName = () => `Guest ${Math.floor(Math.random() * 1000)}`;
+const generateDefaultPlayerName = () => `Spieler ${Math.floor(Math.random() * 1000)}`;
 
 const defaultPlayer = {
     id: uuidv4(),
     name: generateDefaultPlayerName(),
     // foundWords: [], // no default words for production
-    foundWords: ["Anton", "Nena", "Ast"], // default words for testing
-    // Score: 300, // no default score for production
-    Score: 300, // default score for testing
+    foundWords: process.env.NODE_ENV === 'development' ? ["Anton", "Nena", "Ast"] : [],
+    score: process.env.NODE_ENV === 'development' ? 300 : 0,
 };
 
 // use of GameProvider see at end of this chat: https://chat.openai.com/share/9decdb18-a8f4-4783-8f99-4a69dbcc903a
@@ -39,7 +38,7 @@ const GameProvider = ({ children }) => {
             id: uuidv4(),
             name: generateDefaultPlayerName(),
             foundWords: [],
-            Score: 0,
+            score: 0,
         }
 
         addPlayer(defaultPlayer);
