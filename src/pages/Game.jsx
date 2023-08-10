0a2de1388810a5dda6ede3d20091dfc8d35f2bec
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-10 09:35:16 +0200

    Game: Show big button to. Klick starts game and makes different elements visible.

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,9 +1,11 @@
 // file Game.jsx
 
-import React, { useContext } from "react"
+import React, { useState, useContext } from "react"
 import { Link } from "react-router-dom";
 import GameContext from './GameContext';
 import PageTitle from './PageTitle';
+// import app css
+import '../styles/app.css';
 
 // Main content of the page
 export default function Game() {
@@ -13,6 +15,12 @@ export default function Game() {
     // Determine the next player's name for greeting
     const nextPlayerName = players.length > 0 ? players[0].name : "Spieler";
 
+    // State für die Sichtbarkeit des Inhalts
+    const [contentVisible, setContentVisible] = useState(false);
+
+    const handleButtonClick = () => {
+        setContentVisible(true);
+    };
 
     return (
         <div className="welcome ms-5 me-5">
@@ -27,23 +35,33 @@ export default function Game() {
 
             {/* Aktuelle Spieler nennen */}
             <div className="card mt-3">
-            <div className="card-body d-flex flex-column align-items-center">
-                <h2 id="nextPlayerMessage" className="card-title mb-4">Hallo {nextPlayerName}!</h2>
-            </div>
-            <div className="card-body d-flex flex-column align-items-center">
-                {/* Use d-flex for horizontal listing of player names */}
-                <div id="allPlayerNames" className="card-body d-flex justify-content-start flex-wrap">
-                    {players.map((player, index) => (
-                        <span key={player.id} className={index !== players.length - 1 ? "me-3" : ""}>
-                            {player.name}
-                        </span>
-                    ))}
+                <div className="card-body d-flex flex-column align-items-center">
+                    <h2 id="nextPlayerMessage" className="card-title mb-4">Hallo {nextPlayerName}!</h2>
+                </div>
+                <div className="card-body d-flex flex-column align-items-center">
+                    {/* Use d-flex for horizontal listing of player names */}
+                    <div id="allPlayerNames" className="card-body d-flex justify-content-start flex-wrap">
+                        {players.map((player, index) => (
+                            <span key={player.id} className={index !== players.length - 1 ? "me-3" : ""}>
+                                {player.name}
+                            </span>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
 
-            {/* Worteingabe */}
-            <div id="wordInputArea" className="card mb-4">
+            {/* Großer Button */}
+            {!contentVisible &&
+                <div className="d-flex justify-content-center align-items-center vh-20 m-5">
+                    <button className="btn btn-primary btn-lg rounded-circle big-round-button" onClick={handleButtonClick}>
+                        Los geht’s!
+                    </button>
+                </div>
+            }
+
+            {/* Worteingabe - nur sichtbar, wenn contentVisible true ist */}
+            <div id="wordInputArea" className="card mb-4" style={{ display: contentVisible ? 'block' : 'none' }}>
+
                 <div className="card-body">
                     <h2 className="card-title">Wort hinzufügen</h2>
                     <div className="input-group input-button-container">
@@ -63,8 +81,8 @@ export default function Game() {
                 </div>
             </div>
 
-            {/* Gefundene Wörter */}
-            <div id="foundWordsList" className="card mb-4">
+            {/* Gefundene Wörter - nur sichtbar, wenn contentVisible true ist */}
+            <div id="foundWordsList" className="card mb-4" style={{ display: contentVisible ? 'block' : 'none' }}>
                 <div className="card-body">
                     <h2 className="card-title">Gefundene Wörter</h2>
                     <p className="text-center mb-2 fs-5">Wortschlange anzeigen</p>
@@ -72,8 +90,8 @@ export default function Game() {
                 </div>
             </div>
 
-            {/* Spiel beenden */}
-            <div id="finishGameArea" className="text-center">
+            {/* Spiel beenden - nur sichtbar, wenn contentVisible true ist */}
+            <div id="finishGameArea" className="text-center" style={{ display: contentVisible ? 'block' : 'none' }}>
                 <Link to="/game-end" className="btn btn-danger btn-lg mt-5 mb-3">Spiel beenden</Link>
             </div>
 
