ccc82938c39ad06ed52479cd34829acabb650001
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-10 19:55:57 +0200

    GameEnd: Show team and player results, calculate score

--- src/pages/GameEnd.jsx
+++ src/pages/GameEnd.jsx
@@ -25,25 +25,38 @@ export default function GameEnd() {
             setFoundWords(JSON.parse(storedFoundWords));
         }
     }, []); // Empty dependency array to run only once
+
+    const calculateScore = (words) => {
+        const wordCount = words.length;
+        const letterCount = words.reduce((acc, word) => acc + word.length, 0);
+        return wordCount * 100 + letterCount;
+    };
+
+    const totalScore = calculateScore(foundWords);
+    const teamNames = players.map(player => player.name).join(", ");
+
     return (
         <div className="welcome ms-5 me-5">
             <PageTitle title="Das Spiel ist vorbei!" subtitle="Auswertung der Wörtersuche" />
 
             <div id="teamResultArea" className="card mb-4">
                 <div className="card-body text-center">
-                    <h4 className="card-title">Teamergebnis von <span id="teamDisplay"></span>!</h4>
-                    <p>Das Team hat <strong className="text-primary"><span id="wordCountDisplay"></span> Worte</strong> gefunden.</p>
-                    <p>Dabei hat es <strong className="text-primary"><span id="teamScoreDisplay"></span> Punkte</strong> erreicht.</p>
+                    <h4 className="card-title">Teamergebnis von {teamNames}!</h4>
+                    <p>Das Team hat <strong className="text-primary">{foundWords.length} Worte</strong> gefunden.</p>
+                    <p>Dabei hat es <strong className="text-primary">{totalScore} Punkte</strong> erreicht.</p>
                 </div>
             </div>
 
-            <div id="playersResultsArea" className="card mb-4">
-                <div id="playerIndex0ResultArea" className="card-body text-center">
-                    <h4 className="card-title">Ergebnis von <span id="playerIndex0Name"></span>!</h4>
-                    <p>Du hast <strong className="text-primary"><span id="playerIndex0WordCountDisplay"></span> Worte</strong> gefunden.</p>
-                    <p>Dabei hast du <strong className="text-primary"><span id="playerIndex0ScoreDisplay"></span> Punkte</strong> erreicht.</p>
+            {/* Display results for each player */}
+            {players.map((player, index) => (
+                <div key={index} className="card mb-4">
+                    <div className="card-body text-center">
+                        <h4 className="card-title">Ergebnis von {player.name}!</h4>
+                        <p>Du hast <strong className="text-primary">{player.words?.length || 0} Worte</strong> gefunden.</p>
+                        <p>Dabei hast du <strong className="text-primary">{calculateScore(player.words || [])} Punkte</strong> erreicht.</p>
+                    </div>
                 </div>
-            </div>
+            ))}
 
             <div id="playAgainArea" className="card">
                 <div className="card-body d-flex flex-column align-items-center">
