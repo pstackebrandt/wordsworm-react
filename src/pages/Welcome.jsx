d1f00fb30838f2cfd53dbd4e854b8df69801c1bc
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-01-21 20:32:27 +0100

    Fix: Correct subtitle text in Welcome component and remove unused card body div

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -10,10 +10,9 @@ export default function Welcome() {
 
     return (
         <div className="welcome ms-5 me-5">
-            <PageTitle title="Wörterwurm " subtitle="Lasse den Wurm wachsen!" />
+            <PageTitle title="Wörterwurm " subtitle="Lass den Wurm wachsen!" />
 
             <div className="card mt-3">
-                <div className="card-body d-flex flex-column align-items-center"></div>
                 <p className="text-center mb-2 fs-5">Wörterwurm ist ein packendes Spiel, bei dem die Spieler einzeln oder in Teams darum wetteifern eine möglichst große Anzahl von Wörtern zu finden, die in einem bestimmten Zusammenhang stehen.</p>
                 <p className="text-center mb-2 fs-5">Bei jedem gefundenen Wort erfolgt eine spannende Zwischenauswertung, bevor der nächste Spieler seinen Zug beginnt. </p>
                 <p className="text-center mb-2 fs-5">Die entdeckten Wörter formen zusammen den "Wörterwurm", der mit jeder Ergänzung wächst und mehr Punkte für das Team und die individuellen Spieler generiert.</p>
